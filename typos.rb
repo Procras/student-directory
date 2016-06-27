b6a9b89b0df22327f8da99ce5c6f6ee7c045fee9
Giancarlo Fabrizi <gfabrizi@Giancarlos-Air.lan>
2016-06-27 12:45:22 +0200

    missed typo

--- typos.rb
+++ typos.rb
@@ -6,7 +6,7 @@ def input_students
   #get the first name
   name = gets.chomp
   #while the name is not empty, repeat this code
-when !name.empty? do
+while !name.empty? do
   #add the student hash to the array
   students << {name:  name, cohort: :november}
   puts "Now we have #{students.count} students"
